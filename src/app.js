9503c76a483b416aa31199fca19700e71519d827
Andrew Wyper <andy_wyper@yahoo.co.uk>
2020-01-14 20:04:01 +0000

    have retrieved rates from API

--- src/app.js
+++ src/app.js
@@ -4,10 +4,18 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
+      exchangeRates: {},
       amountInEuros: null
     },
+    mounted() {
+      this.getRates()
+    },
     methods: {
-
+      getRates: function() {
+        fetch('https://api.exchangeratesapi.io/latest')
+          .then(response => response.json())
+          .then(dataObject => this.exchangeRates = dataObject)
+      }
     }
   });
 });
